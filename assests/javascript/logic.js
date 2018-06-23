32f720b59642eb228bbe62a2b0128269bd9ef6f9
stannous2 <stannous2@yahoo.com>
2018-06-23 16:24:00 -0700

    updates

--- assests/javascript/logic.js
+++ assests/javascript/logic.js
@@ -5,58 +5,57 @@ let rating = '';
 let stillImage = '';
 let animatedImage = '';
 
+
 createButtons();
+addTopic();
+
+
+function addTopic (){
+    $('#add-topic').on('click', function(){
+        event.preventDefault();
+        let newTopic = $('#topic-input').val().trim();
+        if(!topics.includes(newTopic)) {
+            topics.push(newTopic);
+            createButtons();
+            $('#topic-input').val('');
+        }else
+            return;
+    })
+  }
 
 function createButtons() {
+    $('.topic-button').empty();
     for (i = 0; i < topics.length; i++) {
-        //create a div to hold all buttons
-        let buttonDiv = $('<div>');
-        buttonDiv.addClass('button-bar');
-        console.log('topic name - ' + topics[i])
-
         //create a button
-        let topicButton = $('<button>');
+        let a = $('<button>');
 
         // add attributes to the button
-        topicButton.text('value', topics[i]);
-        topicButton.attr('id', i);
-        topicButton.addClass('btn btn-primary topicButtons');
-
-        console.log('class - ' + topicButton.attr('class'))
+        a.text(topics[i]);
+        a.attr('id', i);
+        a.addClass('btn btn-info');
 
         // append button to the button div
-        buttonDiv.append(topicButton)
-
+        $('.topic-button').append(a)
     }
-
-
 }
 
-
-$('.button-bar').on('click', '.btn', function (e) {
+$('.topic-button').on('click', '.btn', function (e) {
 
     let selectedButton = $(this).html();
 
     //clear the images div everytime the button is clicked
-    $('.images').html('');
-
-    console.log('selected button: ' + selectedButton)
+    $('.imageDiv').html('');
 
     let queryURL = URL + selectedButton;
     console.log('queryURL = ' + queryURL);
     $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
+            url: queryURL,
+            method: "GET"
+        })
         //after the data from the AJAX request comes back
         .then(function (response) {
-            //saving the image_original_url property
 
             for (i = 0; i < response.data.length; i++) {
-                // uniKey.push(response.data[i].id);
-                // console.log('key - ' + uniKey);
-
-
                 // getting still image URL
                 stillImage = 'https://media' + i + '.giphy.com/media/' + response.data[i].id + '/200_s.gif';
                 console.log('stillImage URL - ' + stillImage);
@@ -67,6 +66,7 @@ $('.button-bar').on('click', '.btn', function (e) {
 
                 // getting rating for each giphy
                 rating = response.data[i].rating;
+                rating = 'Rating: ' + rating.toUpperCase();
                 console.log('rating - ' + rating);
 
                 //creating an div storing an image tag
@@ -80,22 +80,21 @@ $('.button-bar').on('click', '.btn', function (e) {
                 image.attr('data-state', 'still');
 
                 //prepeding the retrieved Giphy images to the images div
-                $('.images').prepend(image);
+                $('.imageDiv').prepend(image, rating);
+
             }
         })
 });
 
-
-$(".images").on("click", ".gif", function () {
+$(".imageDiv").on("click", ".gif", function () {
     // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
     let state = $(this).attr("data-state");
-    console.log('image state - ' + state);
+
     // If the clicked image's state is still, update its src attribute to what its data-animate value is.
     // Then, set the image's data-state to animate
     // Else set src to the data-still value
     if (state === "still") {
         $(this).attr("src", $(this).attr("data-animate"));
-        console.log('imgSource - ' + $(this).attr('src'));
         $(this).attr("data-state", "animate");
     } else {
         $(this).attr("src", $(this).attr("data-still"));
